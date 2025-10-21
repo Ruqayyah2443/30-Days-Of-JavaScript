ac2e130b995d13ae3142c91fd15ed0aad5ef5320
Ruqayyah <ruqayyahadebanjo22@gmail.com>
2025-10-21 05:08:07 +0100

    Deleted some content met in variable.js. Created datatype.js and wrote code in main.js of day 1

---
+++ 01_Day_Introduction/01_day_starter/datatypes.js
@@ -0,0 +1,13 @@
+
+//5 Check the data type of each variable
+let name = "Adebano Ruqayyah"
+let food = true
+let emptyValue = null
+let undefinedValue
+console.log(name)
+console.log(food)
+console.log(emptyValue)
+
+console.log(typeof(name))
+console.log(typeof(food))
+console.log(typeof(emptyValue))
--- 01_Day_Introduction/01_day_starter/main.js
+++ 01_Day_Introduction/01_day_starter/main.js
@@ -1,4 +1,41 @@
-// the variable values can be accessed from other variable.js file
-console.log(firstName, lastName, country, city, age, isMarried) 
-console.log(gravity, boilingPoint, PI) // 9.81, 100, 3.14
-console.log(name, job, live)
+// 1.Write a single line comment which says, comments can make code readable
+// comments can make code readable
+
+// 2.
+// Welcome to 30DaysOfJaavaScript
+
+// 3
+/*comments can make code readable, easy to reuse and informative*/
+
+
+// 
+//6 Declare four variables without assigning values
+let planet;
+let todoList;
+let cart;
+let state;
+
+//7 Declare four variables with assigned values
+let food1 = "rice"
+let fruit = "Orange"
+let car = "Benz"
+let footBall = "Real Madrid"
+
+//8 Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+const fName = "Ruqayyah"
+const lName = "Adebanjo"
+let married = true
+const country = "Nigeria"
+let age = 30
+
+
+//9 Declare variables to store personal details (single line)
+const firstName = "Ruqayyah", lastName = "Adebanjo", isMarried = true, myCountry = "Nigeria", myAge2 = 30
+
+/* 10 Declare two variables myAge and yourAge and assign them initial values and log to the browser console. */
+
+let myAge = 30
+let yourAge = 50
+console.log(`I am ${myAge} years old`)
+ console.log(`I am ${yourAge} years old`)
